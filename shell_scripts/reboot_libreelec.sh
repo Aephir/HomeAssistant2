97a7744b8691ec6dea5b5ffe4c5c65b838530bfa
Aephir <waldenb@gmail.com>
2017-11-09 14:43:18 +0100

    version1.2

--- shell_scripts/reboot_libreelec.sh
+++ shell_scripts/reboot_libreelec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i /home/homeassistant/connection/privatekey_libreelec root@192.168.0.155 "shutdown -h now"
+ssh -p 155 -i /home/homeassistant/connection/privatekey_libreelec pi@192.168.0.155 "sudo reboot"
