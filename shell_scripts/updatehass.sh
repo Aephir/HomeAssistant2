b94268ff824eaaedc3b74e499d24113c8dca64f9
Aephir <waldenb@gmail.com>
2018-04-10 20:42:13 +0200

    updated notify & floorplan

--- shell_scripts/updatehass.sh
+++ shell_scripts/updatehass.sh
@@ -4,16 +4,21 @@
 #!/bin/bash
 
 # Stop Home Assistant
+echo "stopping home assistant"
 sudo systemctl stop home-assistant@homeassistant.service
 
 # Become user 'hass'
+echo "Become user homeassistantn"
 sudo su -s /bin/bash homeassistant <<'EOF'
 # Activate the virtualenv
+echo "activate the virtual environment"
 source /srv/homeassistant/bin/activate
 # Install Home Assistant
+echo "install home assistant"
 pip3 install --upgrade homeassistant
 exit
 EOF
 
 # Restart Home Assistant
+echo "start home assistant"
 sudo systemctl restart home-assistant@homeassistant.service
